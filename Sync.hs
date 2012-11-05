92a82eb3639d201985bbeafba758680e96f5a8e6
Nick Straw <nick@broxholme.com>
2012-11-05 22:01:00 +0000

    First commit

---
+++ Sync.hs
@@ -0,0 +1,192 @@
+
+module Sync
+    (
+        saferGetModificationTime
+        , file2fileStats
+        , dontCopyFile
+        , toDelete
+        , toCopy
+        , copy
+        , rm
+        , getDir
+        , saferFileSize
+        , doNothing  
+        , FileStats(..)
+        , Dir(..)
+        , mdtm
+        , Path(..)
+        , FtpPath(..)
+        , bookclub
+        , open_bookclub
+        , sync
+        , seqList
+    ) where
+
+import System.IO
+import Control.Monad (liftM)
+import Control.Exception (handle)
+import Data.List (deleteFirstsBy)
+import System.Directory
+--import System.FilePath.Posix (replaceDirectory)
+import Data.Time.Clock (UTCTime)
+import Network.FTP.Client
+import Network.FTP.Client.Parser
+import Data.List.Utils (seqList)
+-- My date conversion routines
+import Date
+
+data Path = Ftp FtpPath | Local FilePath
+
+instance Show Path where
+    show p = case p of
+                Ftp fp -> address fp
+                Local fp -> fp
+                
+instance Show FTPConnection where
+    show = const "This is an FTP connection"
+
+data FtpPath = FtpPath {  address::String
+                        , usr::String
+                        , pwd::String
+                        , conn::FTPConnection
+                        , res::FTPResult
+                     } deriving (Show)
+                     
+data FileStats = FileStats {fname::FilePath, fsize::(Maybe Integer), fdate::Maybe UTCTime} deriving (Show)
+data Dir = Dir {path::Path, fs::[FileStats]} deriving (Show)
+
+open_bookclub::FilePath->IO (Dir, Dir)
+open_bookclub pp = do
+        let brox_address = "ftp1.namesco.net"
+        let brox_usr = "broxholme.com"
+        let brox_pwd = "55s1nc3r1ty"
+        brox_conn <- easyConnectFTP brox_address
+        brox_res1 <- login brox_conn brox_usr (Just brox_pwd) Nothing
+        brox_res2 <- cwd brox_conn $ "./web/" ++ pp ++ "/"        
+        let brox = FtpPath brox_address brox_usr brox_pwd brox_conn brox_res2
+        rdir<-getDir $ Ftp brox
+        let loc = Local $ "C:\\Documents and Settings\\Robin Seeley\\workspace\\Bookclub\\_site\\" ++ pp ++ "\\"
+        ldir<-getDir loc        
+        return (rdir, ldir)
+
+bookclub::IO [()]
+bookclub = do
+        let ds = ["pages", "css", "xml"]
+        mapM (\d-> do
+                    (rdir, ldir) <- open_bookclub d
+                    sync ldir rdir
+                    return ()
+            ) ds
+
+-- This is how to use these functions
+sync::Dir->Dir->IO [()]
+sync new old = do
+    copy (toCopy new old) old
+    rm (toDelete old new)
+    
+-- currently old must be local!!!
+copy::Dir->Dir->IO [()]
+copy new old = do
+    setCurrentDirectory $ show $ path new
+    case path old of
+        Ftp opath -> sequence $ map (\fs-> do 
+                                            r<-uploadbinary (conn opath) (fname fs)
+                                            return ()
+                                    ) (seqList $ fs new)
+        Local opath -> sequence $ map (\fs->copyFile ((show $ path new) ++ fname fs) ((show $ path old) ++ fname fs)) (fs new)
+    
+rm::Dir->IO [()]
+rm old = do
+    case path old of
+        Local opath -> sequence $ map (\f-> removeFile $ opath ++ fname f) (fs old)
+        Ftp opath -> sequence $ map (\f-> do
+                                            r<-delete (conn opath) (fname f)
+                                            return ()
+                                    ) (seqList $ fs old)
+    
+
+-- | Gets the modification time of a file over an FTP connection
+-- | Wraps the time in a @Maybe@ so that non existant files are caught
+-- Has to be in IO because of getNodificationTime
+mdtm :: (Num a, Read a) => FTPConnection -> String -> IO a
+mdtm h fn = do
+            r <- sendcmd h ("MDTM " ++ fn)
+            forceioresp 200 r
+            return (read . head . snd $ r)
+
+
+           
+-- | Wraps the time in a @Maybe@ so that non existant files are caught
+-- Has to be in IO because of getNodificationTime
+saferGetModificationTime::FilePath->IO (Maybe UTCTime)
+saferGetModificationTime f = do
+    ok <- doesFileExist f
+    case ok of
+        True -> do
+                    ct <- getModificationTime f 
+                    return $ Just $ clock2UTC ct
+        False -> return Nothing
+      
+-- | Conversion of filepath to FileStats      
+-- Has to be in IO because of getNodificationTime and saferFileSize
+file2fileStats::Path->FilePath->IO FileStats
+file2fileStats p f =
+    case p of
+        Local pname ->  do 
+                            sz <- saferFileSize $ pname ++ f
+                            tm <- saferGetModificationTime $ pname ++ f 
+                            return (FileStats f sz tm)
+        Ftp pname   -> do
+                            sz <- size (conn pname) f
+                            tm <- mdtm (conn pname) f
+                            return (FileStats f (Just sz) (Just $ ftpInt2UTC tm))
+    
+-- | Don't copy returns false when the new file is older than the old one
+dontCopyFile::FileStats->FileStats->Bool
+dontCopyFile new old = (fname new == fname old) && (fdate new <= fdate old)
+    
+-- | Creats a list of all the files in old that not in the new list
+-- These are the files that eneed to be deleted
+toDelete::Dir->Dir->Dir
+toDelete (Dir n []) _ = Dir n[]
+toDelete old (Dir n []) = old
+toDelete old new = Dir (path old) $ deleteFirstsBy (\o n -> fname n == fname o) (fs old) (fs new)
+
+-- | Creates a list of all the files that need to be copied
+-- This will be all the ones that don't satisfy dontCopyFile
+toCopy::Dir->Dir->Dir
+toCopy (Dir n []) _ = Dir n []
+toCopy new (Dir n []) = new
+toCopy new old = Dir (path new) $ deleteFirstsBy (\o n -> (fname n == fname o) && (fdate n <= fdate o)) (fs new) (fs old)
+
+-- | Has to be in IO because of getDirectoryContents
+-- handles potential errors in getDirectoryContents
+-- FilePath should include the trailing '/'
+getDir::Path->IO Dir
+getDir p = handle doNothing2 $ do 
+    case p of
+        Ftp fp -> do 
+                    files <- nlst (conn fp) Nothing
+                    fs <- sequence $ map (file2fileStats p) $ seqList files 
+                    return $ Dir p fs
+        Local lpath -> do 
+                        files <- liftM (filter (\x-> (x/="." && x/=".."))) $ getDirectoryContents lpath
+                        fs<- sequence $ map (file2fileStats p) files 
+                        return $ Dir p fs
+  
+-- | Has to be in IO because we have to open the file
+-- handles potential errors in openFile
+saferFileSize :: FilePath -> IO (Maybe Integer)
+saferFileSize path = handle doNothing $ do
+  h <- openFile path ReadMode
+  size <- hFileSize h
+  hClose h
+  return (Just size) 
+   
+-- I think handle catches erorrs - so this just returns Nothing when it does
+doNothing :: IOError -> IO (Maybe a)
+doNothing e = return Nothing    
+
+-- I think handle catches errors - so this just returns Nothing when it does
+doNothing2 :: IOError -> IO Dir
+doNothing2 e = return (Dir (Local "") [])
